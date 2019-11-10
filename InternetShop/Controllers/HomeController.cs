9f48b46b2f773c4971a0dabff1af69d9b78dd611
RSB <s_astana@mail.ru>
2019-11-11 01:05:57 +0600

    first commit

---
+++ InternetShop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InternetShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            ViewBag.Message = "Список продуктов";
+
+            using (var context = new Context())
+            {
+                ViewBag.Products = context.Products.ToList();
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ InternetShop/DataInitializer.cs
@@ -0,0 +1,34 @@
+﻿using InternetShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace InternetShop
+{
+    public class DataInitializer : CreateDatabaseIfNotExists<Context>
+    {
+        protected override void Seed(Context context)
+        {
+            context.Users.AddRange(new List<User>{
+                new User
+                {
+                    Login = "Admin",
+                    Password = "123456"
+                },
+                new User
+                {
+                    Login = "User1",
+                    Password = "100001"
+                },
+                new User
+                {
+                    Login = "User2",
+                    Password = "100002"
+                }
+            });
+            context.SaveChanges();
+        }
+    }
+}
